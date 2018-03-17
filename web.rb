deaaa8a931ef5bb7b071eebd7ff8953b7a286575
Eric Martinez <eric.michael.mtz@gmail.com>
2018-03-17 16:15:33 -0500

    Fix bug

--- web.rb
+++ web.rb
@@ -6,22 +6,11 @@ require 'openssl'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 def get_search_count(term)
-	url = "https://www.udemy.com/api-2.0/search-suggestions?q=#{term}"
-	uri = URI(url)
-	response = Net::HTTP.get(uri)
-	udemy_hash = JSON.parse(response)
-	return udemy_hash["results"][0]["searched_count"]
+# your code here
 end
 
 
 get '/' do
-	if params["term"] && !params["term"].empty?
-		count = get_search_count(params["term"])
-		return "#{params['term']}: #{count}"
-	elsif params["term"] && params["term"].empty?
-		return "Error: Search Term Empty"
-	else
-		return "Error: No Search Term Provided"
-	end
+# your code here
 end
 
